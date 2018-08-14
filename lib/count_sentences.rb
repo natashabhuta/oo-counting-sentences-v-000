ab93d4cb5e7e02442ee1d4b974ebba5a6e6d9d3b
natashabhuta <natasha.bhuta@gmail.com>
2018-08-14 01:08:33 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,10 +3,7 @@ require 'pry'
 class String
 
   def sentence?
-    if self.end_with?(".") 
-      return true
-     else 
-       return false
+    if self.end_with?(".") ? true : false 
   end 
   end
 
