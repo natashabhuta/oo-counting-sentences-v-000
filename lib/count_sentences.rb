5ff18c75712d7c5f144a5f007abe8438982fb10b
natashabhuta <natasha.bhuta@gmail.com>
2018-08-14 01:09:29 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -5,14 +5,17 @@ class String
   def sentence?
     if self.end_with?(".") ? true : false 
   end
+end 
 
   def question?
-if self.end_with?("?") ? true : false 
+  if self.end_with?("?") ? true : false 
   end
+end 
 
   def exclamation?
   if self.end_with?("!") ? true : false 
   end
+end 
 
   def count_sentences
 
