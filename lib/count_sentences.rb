fec21e1c29e086d96bfa299cfd4bb6e3b31b37b0
natashabhuta <natasha.bhuta@gmail.com>
2018-08-14 01:16:10 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,34 @@
 require 'pry'
 
 class String
-
-  def sentence?
-    
+ 
+ def sentence?
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
-
+  
   def count_sentences
-
+  sentence_array = self.split(/[.?!]/)
+    sentence_array.delete_if{|sentence| sentence.empty?}
+    sentence_array.length
   end
 end
